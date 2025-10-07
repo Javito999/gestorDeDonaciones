8925a8810796b550391cc007821811999d83f6ed
Javito999 <patxixav9>
2025-10-07 20:19:41 +0200

    finalizado

--- funcionalidad/donaciones.js
+++ funcionalidad/donaciones.js
@@ -51,5 +51,35 @@ function finalizarTramite() {
     .sort((a, b) => b.nombre.localeCompare(a.nombre)); 
 
     
+    asociacionesOrdenadas.forEach(a => {
+       let texto = a.nombre + "----" + a.cantidad + " ";
+       if(a.cantidad === 1){
+            texto+= "aportación\n";
+       }else {
+            texto += "aportaciones\n";
+       }
+       salida += texto;
+    });
+
+   
+    let suma = donaciones.reduce((total, valor) => total + valor, 0);
+    salida += "Donación final: " + suma + " €\n";
+
+   
+    let media = (suma / contarDonaciones).toFixed(2);
+    salida += "Donación media: " + media + " €/aportación";
+
+    area.value = salida;
+
+    
+    finalizado = true;
 }
+
+
+function limpiarProceso() {
+    donaciones = [];
+    contarDonaciones = 0;
+    contadorAsociaciones = new Array(asociaciones.length).fill(0);
+}
+
 
